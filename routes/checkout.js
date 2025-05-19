1ac87febabfac79e7699b57c503a884a0d783ae8
Alexis5900 <alexisxm1492@hotmail.com>
2025-05-19 03:00:45 -0400

    Descripción de los cambios

--- routes/checkout.js
+++ routes/checkout.js
@@ -52,8 +52,8 @@ router.post('/create-checkout-session', async (req, res) => {
       payment_method_types: ['card'],
       line_items,
       mode: 'payment',
-      success_url: 'http://localhost:5173/success',
-      cancel_url: 'http://localhost:5173/cart'
+      success_url: `${process.env.FRONTEND_URL}/success`,
+      cancel_url: `${process.env.FRONTEND_URL}/cart`
     })
 
     // Guardar la compra en la base de datos
